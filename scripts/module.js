c0418ca00c8b4b82cfe16ea2481234d01215d0a8
Büsra Sahin <buesra.sahin97@web.de>
2023-07-09 23:31:53 +0200

    add Todos

--- scripts/module.js
+++ scripts/module.js
@@ -1,10 +1,13 @@
 Hooks.on("init", () => {
+  // TODO: register templates
+  // CONFIG.debug.hooks = true; // See all available Hooks in console
   console.log(
     "This code runs once the Foundry VTT software begins its initialization workflow."
   );
 });
 
 Hooks.on("ready", () => {
+  // TODO: inject styles
   console.log(
     "This code runs once core initialization is ready and game data is available."
   );
